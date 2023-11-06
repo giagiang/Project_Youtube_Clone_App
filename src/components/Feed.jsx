30935545d084aed66e425457f636d7d37c70c9f3
fastboy <giagiang200@gmail.com>
2023-11-06 15:47:37 +0700

    create sidebar and make color

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,18 +1,38 @@
 import { useState, useEffect } from "react";
 import { Stack, Box, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx = {{height: {sx:'auto',md: '92vh'},
-      borderRight:'1px solid #3d3d3d', px : {sx:0, md:2}}}>
-        <Sidebar/>
-        <Typography className="copyright"
-        variant="body2" sx= {{mt:1.5, color:'#fff'}} >
-              Copyright by Ronnie Ric G 
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright by Ronnie Ric G
         </Typography>
       </Box>
-    </Stack> 
+      <Box>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "red" }}>videos</span>
+        </Typography>
+        <Videos/>
+      </Box>
+    </Stack>
   );
 };
 
