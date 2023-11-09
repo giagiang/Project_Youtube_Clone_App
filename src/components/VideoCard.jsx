ffe179455b0a7de139377683daf5ce7d137ebe8c
fastboy <giagiang200@gmail.com>
2023-11-09 22:51:46 +0700

    create-videoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
-const VideoCard = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoChannelUrl,
+  demoVideoUrl,
+  demoChannelTitle,
+  demoVideoTitle,
+  demoProfilePicture,
+} from "../utils/constants";
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  console.log("props in videosCard" , {videoId, snippet})
   return (
-    <div>VideoCard</div>
-  )
-}
+    <Card>
+      <Link to={videoId ? `./video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.hight?.url || demoThumbnailUrl}
+          alt={snippet?.title}
+          sx={{ width: { xs: "100%", sm: "358px" }, height: "180px" }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }} />
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Typography variant="subtitle1" fontWeight="bold" color="black">
+          {" "}
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+    </Card>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
