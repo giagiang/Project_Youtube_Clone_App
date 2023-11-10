284e8d7235bab8f1b913cf94f4b10d7de3dcda53
fastboy <giagiang200@gmail.com>
2023-11-10 16:36:08 +0700

    add-channelCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,7 +8,7 @@ import {
   demoVideoUrl,
   demoChannelTitle,
   demoVideoTitle,
-  demoProfilePicture,
+  // demoProfilePicture,
 } from "../utils/constants";
 const VideoCard = ({
   video: {
@@ -18,18 +18,17 @@ const VideoCard = ({
 }) => {
   console.log("props in videosCard", { videoId, snippet });
   return (
-    <Card sx= {{width: {md: '320px', xs : '100%'}, boxShadow: 'none', borderRadius : 0}}>
+    <Card sx= {{width: {md: '300px', xs : '100%'}, boxShadow: 'none', borderRadius : 3}}>
       <Link to={videoId ? `./video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.hight?.url || demoThumbnailUrl}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width: { xs: "100%", sm: "358px" }, height: "180px" }}
+          sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
         />
       </Link>
-      <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }} />
+      <CardContent sx={{ backgroundColor: "#1E1E1E" }} />
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <Typography variant="subtitle1" fontWeight="bold" color="black">
-          {" "}
+        <Typography variant="subtitle1" fontWeight="bold"  color="black">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
@@ -40,10 +39,9 @@ const VideoCard = ({
             : demoChannelUrl
         }
       >
-        <Typography variant="subtitle2" fontWeight="bold" color="gray">
-          {" "}
+        <Typography variant="subtitle2" fontWeight="bold" color="gray" height= '65px'>
           {snippet?.channelTitle || demoChannelTitle}
-          <CheckCircle sx = {{fontSize :12, color: 'blue', ml:'5px'}}></CheckCircle>
+          <CheckCircle sx = {{fontSize :12, color: 'gray', ml:'5px'}}></CheckCircle>
         </Typography>
       </Link>
     </Card>
